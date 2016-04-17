58167a4e84348324287a38f916476b9f1a04c3f9
Fogre <antti.kotiranta@helsinki.fi>
2016-04-17 15:50:00 +0300

    Viikko 4 done.

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
     public static void main(String[] args) {
-        WebDriver driver = new HtmlUnitDriver();
+        WebDriver driver = new FirefoxDriver();
 
         driver.get("http://localhost:8090");
         System.out.println( driver.getPageSource() );
@@ -26,6 +26,58 @@ public class Tester {
         
         System.out.println("==");
         System.out.println( driver.getPageSource() );
+
+
+        // epäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana
+        driver.get("http://localhost:8090");
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("vaarasalasana");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        System.out.println(driver.getPageSource());
+
+        // epäonnistunut kirjautuminen: ei-olemassaoleva käyttäjätunnus
+        driver.get("http://localhost:8090");
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("vaara");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        System.out.println(driver.getPageSource());
+
+        // uuden käyttäjätunnuksen luominen
+        driver.get("http://localhost:8090");
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("uusitunnus");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("uusI11");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("uusI11");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        System.out.println(driver.getPageSource());
+        
+        // uuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        System.out.println(driver.getPageSource());
         
     }
 }
